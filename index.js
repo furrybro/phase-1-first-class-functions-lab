3109acfacf746cf64426b1c9599d9d21f1dfb1c9
Jeena Park <jeenapark15@gmail.com>
2022-04-26 20:55:07 -0400

    commit tree 3f39c8120b58324610995afb0d7440c1a6865bd8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+// const returnFirstTwoDrivers = (drivers) => {
+//     return [drivers[0], drivers[1]]
+// };
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+}
+
+const selectingDrivers = []
+selectingDrivers[0] = returnFirstTwoDrivers;
+selectingDrivers[1] = returnLastTwoDrivers;
+
+function createFareMultiplier(int) {
+    return (fare) => fare * int;
+ }
+
+ const fareDoubler = createFareMultiplier(2);
+
+ const fareTripler = createFareMultiplier(3);
+
+ function selectDifferentDrivers(drivers, driverSelectingFunction) {
+     return driverSelectingFunction(drivers);
+ }
